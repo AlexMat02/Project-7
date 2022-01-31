4783e1fa898249d091a6a2144c1487c96af2941d
AlexMat02 <ourougank@gmail.com>
2022-01-31 11:42:19 +0100

    Added backend getAllUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -81,3 +81,17 @@ exports.deleteUser = (req, res) => {
         }
     )
 };
+
+exports.getAllUser = (req, res) => {
+    User.find().then(
+        (users) => {
+            res.status(200).json(users);
+        }
+    ).catch(
+        (err) => {
+            res.status(400).json({
+                error: err
+            })
+        }
+    )
+}
