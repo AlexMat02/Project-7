52d7587cc7de1f20914f99f2fdf272596c33a1be
AlexMat02 <ourougank@gmail.com>
2022-02-01 16:36:03 +0100

    Updated mainContent to make sure there is a msg if no posts

--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -29,54 +29,63 @@ export default {
         .then(res => localStorage.setItem("postArray", JSON.stringify(res)))
         .then(console.log(" localstorage item ->  " , JSON.parse(localStorage.getItem('postArray'))))
         let posts = JSON.parse(localStorage.getItem('postArray'));
-        for (let n = 0; n < posts.length; n++) {
+        if (posts.length == 0) {
+            console.log("No posts avaible");
             const baseDiv = document.getElementsByClassName("mainContent__container")[0];
-            let mainDiv = document.createElement("div");
-            mainDiv.className = "divContent";
-            let headerDiv = document.createElement("div");
-            headerDiv.className = "divContent__top"
-            let contentDiv = document.createElement("div");
-            contentDiv.className = "divContent__content";
-            let pContent = document.createElement("p");
-            pContent.textContent = posts[n].content;
-            let headerLogoLink = document.createElement("a");
-            headerLogoLink.href = "/profilExample"
-            headerLogoLink.id = "logo" + n; 
-            let headerLogo = document.createElement("img");
-            headerLogo.className = "divContent__profilImg";
-            let headerTitle = document.createElement("h2");
-            headerTitle.className = "divContent__header";
-            headerTitle.textContent = posts[n].title;
-            let headerType = document.createElement("h2");
-            headerType.className = "divContent__header divContent__filter";
-            headerType.innerText = posts[n].type;
-            let link = document.createElement("a");
-            link.href = "/postExample"
-            link.id = n;
-            baseDiv.appendChild(mainDiv);
-            mainDiv.appendChild(headerDiv);
-            headerDiv.appendChild(headerLogoLink);
-            headerLogoLink.appendChild(headerLogo);
-            headerDiv.append(link);
-            link.appendChild(headerTitle);
-            headerDiv.appendChild(headerType);
-            mainDiv.appendChild(contentDiv);
-            contentDiv.appendChild(pContent);
+            let noPostMsg = document.createElement("h2");
+            noPostMsg.innerHTML = "No posts have been found...."
+            noPostMsg.style.margin = "auto";
+            baseDiv.appendChild(noPostMsg);
+        } else {
+            for (let n = 0; n < posts.length; n++) {
+                const baseDiv = document.getElementsByClassName("mainContent__container")[0];
+                let mainDiv = document.createElement("div");
+                mainDiv.className = "divContent";
+                let headerDiv = document.createElement("div");
+                headerDiv.className = "divContent__top"
+                let contentDiv = document.createElement("div");
+                contentDiv.className = "divContent__content";
+                let pContent = document.createElement("p");
+                pContent.textContent = posts[n].content;
+                let headerLogoLink = document.createElement("a");
+                headerLogoLink.href = "/profilExample"
+                headerLogoLink.id = "logo" + n; 
+                let headerLogo = document.createElement("img");
+                headerLogo.className = "divContent__profilImg";
+                let headerTitle = document.createElement("h2");
+                headerTitle.className = "divContent__header";
+                headerTitle.textContent = posts[n].title;
+                let headerType = document.createElement("h2");
+                headerType.className = "divContent__header divContent__filter";
+                headerType.innerText = posts[n].type;
+                let link = document.createElement("a");
+                link.href = "/postExample"
+                link.id = n;
+                baseDiv.appendChild(mainDiv);
+                mainDiv.appendChild(headerDiv);
+                headerDiv.appendChild(headerLogoLink);
+                headerLogoLink.appendChild(headerLogo);
+                headerDiv.append(link);
+                link.appendChild(headerTitle);
+                headerDiv.appendChild(headerType);
+                mainDiv.appendChild(contentDiv);
+                contentDiv.appendChild(pContent);
 
-            let currentPost = document.getElementById(n);
-            currentPost.addEventListener("click", () => {
-                let postChosen = JSON.stringify(n);
-                localStorage.setItem("postNumber" , postChosen);
-                console.log("LOGGED postNumber -> " , JSON.parse(localStorage.getItem("postNumber")));
+                let currentPost = document.getElementById(n);
+                currentPost.addEventListener("click", () => {
+                    let postChosen = JSON.stringify(n);
+                    localStorage.setItem("postNumber" , postChosen);
+                    console.log("LOGGED postNumber -> " , JSON.parse(localStorage.getItem("postNumber")));
+                })
+                let currentProfil = document.getElementById("logo" + n);
+                currentProfil.addEventListener("click", () => {
+                    let profilChosen = posts[n].userId;
+                    localStorage.setItem("profilNumber", profilChosen);
             })
-            let currentProfil = document.getElementById("logo" + n);
-            currentProfil.addEventListener("click", () => {
-                let profilChosen = posts[n].userId;
-                localStorage.setItem("profilNumber", profilChosen);
-            })
+            }
+            const userData = JSON.parse(localStorage.getItem("userData"));
+            this.$store.dispatch('expChecker' , {userData});
         }
-        const userData = JSON.parse(localStorage.getItem("userData"));
-        this.$store.dispatch('expChecker' , {userData});
     }
 }
 </script>
