5d7a51c282fad7ad8c2f64a59c76f4ed3bffc7c7
AlexMat02 <ourougank@gmail.com>
2022-03-28 15:12:59 +0200

    deleted useless code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,3 @@
-const express = require('express');
-
-/* const mongoose = require("mongoose") */
 const mysql = require("mysql");
 const connection = mysql.createConnection({
     host : 'localhost',
@@ -23,13 +20,3 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
-
-/* mongoose.connect('mongodb+srv://user1:RsvpZHgDAJVHmIV5@cluster0.2wy7q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-    .then(() => {
-        console.log('Success, Connected to MONGODB')
-    })
-    .catch((error) => {
-        console.log('Unable to connect to MongoDb Atlas');
-        console.error(error)
-    }
-); */
